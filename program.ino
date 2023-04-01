bbe2e4795c30395533a30bc8894fe51cff74eac6
SbsCruz <sebas.cruz750@gmail.com>
2023-04-01 17:26:16 -0500

    add: main arduino sketch

---
+++ program.ino
@@ -0,0 +1,132 @@
+// Definimos las conexiones de los pines del display de 7 segmentos utilizando #define
+#define A 2
+#define B 3
+#define C 4
+#define D 5
+#define E 6
+#define F 7
+#define G 8
+
+void setup() {
+  // Configuramos los pines del display como salidas
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+
+  // Inicializamos la comunicación serial a 9600 baudios
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Si hay datos disponibles en el búfer de entrada serial, leemos el número decimal ingresado
+  if (Serial.available() > 0) {
+    int numero = Serial.parseInt(); // Leemos el número decimal ingresado por teclado
+
+    // Mostramos el número en el display de 7 segmentos
+    switch(numero) {
+      case 0:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(E, HIGH);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, LOW);
+        break;
+      case 1:
+        digitalWrite(A, LOW);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, LOW);
+        digitalWrite(E, LOW);
+        digitalWrite(F, LOW);
+        digitalWrite(G, LOW);
+        break;
+      case 2:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, LOW);
+        digitalWrite(D, HIGH);
+        digitalWrite(E, HIGH);
+        digitalWrite(F, LOW);
+        digitalWrite(G, HIGH);
+        break;
+      case 3:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(E, LOW);
+        digitalWrite(F, LOW);
+        digitalWrite(G, HIGH);
+        break;
+      case 4:
+        digitalWrite(A, LOW);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, LOW);
+        digitalWrite(E, LOW);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 5:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, LOW);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(E, LOW);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 6:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, LOW);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(E, HIGH);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 7:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, LOW);
+        digitalWrite(E, LOW);
+        digitalWrite(F, LOW);
+        digitalWrite(G, LOW);
+        break;
+      case 8:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(E, HIGH);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 9:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(E, LOW);
+        digitalWrite(F, LOW);
+        digitalWrite(G, HIGH);
+    break;
+  default:
+    // Si se ingresó un número que no está entre 0 y 9, apagamos todos los segmentos del display
+    digitalWrite(A, LOW);
+    digitalWrite(B, LOW);
+    digitalWrite(C, LOW);
+    digitalWrite(D, LOW);
+    digitalWrite(E, LOW);
+    digitalWrite(F, LOW);
+    digitalWrite(G, LOW);
+    break;
+}
+
